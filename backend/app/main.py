0665b6b2f9ee0e837189e05d0164b506b258221c
DineshbalaS <baladinesh763@gmail.com>
2025-06-03 12:11:55 +0530

    connected to DB

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from backend.app.config import db
 
 app = FastAPI()
 
 @app.get('/')
 def read_root():
     return {"message" : "backend is working"}
+
+@app.get("/test-db")
+def test_db():
+    collections = db.list_collection_names()
+    return {"collections": collections}
